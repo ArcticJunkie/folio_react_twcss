89d6c293180e3dabfff9caa6b13e2681bf7a2c16
ArcticJunkie <pikka99@gmail.com>
2020-12-30 03:37:56 +0530

    changed background to yellow, fixed links to open in new tab, added github link to development anchor

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App bg-white">
 
 
-      <main className="flex justify-between space-x-8 px-8 py-10 md:px-52 md:py-20 bg-blue-50 text-gray-800 text-xl font-poppins">
+      <main className="flex justify-between space-x-8 px-8 py-10 md:px-52 md:py-20 bg-yellow-100 text-gray-800 text-xl font-poppins">
         <div className="flex flex-col space-y-6">
           <div className="flex flex-row space-x-8 text-gray-500 text-base">
             <span className="">BRAND-STRATEGY</span>
@@ -16,7 +16,7 @@ function App() {
           <div className="">
             <span>Something something aesthetics and technology -> method + madness idk. Make this longer by adding more words abc abc abc abc abc abc abc </span>
             <div className="py-4">
-              <span className="">Put simply, I <a href="" className="group relative"><span className="relative z-20 italic font-semibold group-hover:text-black ">design</span><i className="absolute opacity-60 z-10 left bottom-1 inset-x-0 bg-blue-300 h-1.5 group-hover:h-3 transition-height duration-300 ease-in-out "></i></a> and <a href="" className="group relative"><span className="relative z-20 font-mono font-semibold group-hover:text-black">develop</span><i className="absolute opacity-60 z-10 left bottom-1 inset-x-0 bg-blue-300 h-1.5 group-hover:h-3 transition-height duration-300 ease-in-out"></i></a>  experiences</span>
+              <span className="">Put simply, I <a href="https://dribbble.com/navs_lists" target="_blank" className="group relative"><span className="relative z-20 italic font-semibold group-hover:text-black ">design</span><i className="absolute opacity-60 z-10 left bottom-1 inset-x-0 bg-blue-300 h-1.5 group-hover:h-3 transition-height duration-300 ease-in-out "></i></a> and <a href="https://github.com/ArcticJunkie" target="_blank" className="group relative"><span className="relative z-20 font-mono font-semibold group-hover:text-black">develop</span><i className="absolute opacity-60 z-10 left bottom-1 inset-x-0 bg-blue-300 h-1.5 group-hover:h-3 transition-height duration-300 ease-in-out"></i></a>  experiences</span>
             </div>
             <div className=" text-sm text-gray-500">
               <span>Job opportunities are warmly welcomed, </span>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,12 +3,12 @@
 function Nav() {
     return (
         <div className="Nav text-gray-700">
-            <nav className="bg-white flex flex-row items-center justify-between px-8 py-8 md:px-52 md:py-8">
+            <nav className="bg-yellow-100 flex flex-row items-center justify-between px-8 py-8 md:px-52 md:py-8">
                 <div className="font-bold font-mono text-2xl hover:text-yellow-500"><a href="#" className="">AV</a></div>
                 <ul className="hidden md:flex flex-row justify-evenly space-x-12 font-mono ">
-                    <li className="hover:underline hover:text-black"><a href="https://www.linkedin.com/in/nav-verma/">LinkedIn</a></li>
-                    <li className="hover:underline hover:text-black"><a href="https://dribbble.com/navs_lists">Dribbble</a></li>
-                    <li className="hover:underline hover:text-black"><a href="https://drive.google.com/file/d/1Y_uy5qveuIf9GfGZcfIXKGOIH1DRb_PM/view?usp=sharing">Resume</a></li>
+                    <li className="hover:underline hover:text-black"><a href="https://www.linkedin.com/in/nav-verma/" target="_blank">LinkedIn</a></li>
+                    <li className="hover:underline hover:text-black"><a href="https://dribbble.com/navs_lists" target="_blank">Dribbble</a></li>
+                    <li className="hover:underline hover:text-black"><a href="https://drive.google.com/file/d/1Y_uy5qveuIf9GfGZcfIXKGOIH1DRb_PM/view?usp=sharing" target="_blank">Resume</a></li>
                 </ul>
             </nav>
         </div>
