e6b0888042ae60f565bb442c0e1e2f5e77a7d07c
ArcticJunkie <pikka99@gmail.com>
2021-01-02 00:31:56 +0530

    added half decent adcopy to landing page, increased line-spacing and body text size, changed bg from yellow to white

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
 import Typical from 'react-typical';
+import { useSpring, animated } from 'react-spring'
 
 function App() {
+  const spring = useSpring({
+    from: {
+      myXTranslateValue: 60,
+    },
+    to: {
+      myXTranslateValue: 0,
+    },
+    config: {
+      mass: 1,
+    },
+  });
   return (
-    <div className="App bg-yellow-100 h-screen">
+    <div className="App bg-white h-screen ">
 
 
-      <main className="flex justify-between space-x-16 px-8 py-10 md:px-52 md:py-20 bg-yellow-100 text-gray-800 text-xl font-poppins">
+      <main className="flex justify-between space-x-16 px-8 pt-10 md:px-52 md:pt-20 bg-white text-gray-800 text-xl font-poppins">
 
         <div className="flex justify-between flex-col space-y-6">
-          <div className="flex flex-col space-y-4">
+          <div className="flex flex-col space-y-10">
 
-            <div className="flex flex-col space-y-2 font-semibold">
+            <div className="flex flex-col space-y-8 font-semibold">
               <span className="">Abhinav Verma</span>
-              <span className="">I <a href="https://dribbble.com/navs_lists" target="_blank" className="group relative"><span className="relative z-20 italic font-semibold group-hover:text-black ">design</span><i className="absolute opacity-60 z-10 left bottom-1 inset-x-0 bg-blue-300 h-1.5 group-hover:h-3 transition-height duration-300 ease-in-out "></i></a> and <a href="https://github.com/ArcticJunkie" target="_blank" className="group relative"><span className="relative z-20 font-mono font-semibold group-hover:text-black">develop</span><i className="absolute opacity-60 z-10 left bottom-1 inset-x-0 bg-blue-300 h-1.5 group-hover:h-3 transition-height duration-300 ease-in-out"></i></a>
+              <span className="">I <a href="https://dribbble.com/navs_lists" target="_blank" className="group relative"><span className="relative z-20 italic font-semibold group-hover:text-black ">design</span><i className="absolute opacity-60 z-10 left bottom-1 inset-x-0 bg-blue-300 h-1.5 group-hover:h-3 transition-height duration-200 ease-in-out "></i></a> and <a href="https://github.com/ArcticJunkie" target="_blank" className="group relative"><span className="relative z-20 font-mono font-semibold group-hover:text-black">develop</span><i className="absolute opacity-60 z-10 left bottom-1 inset-x-0 bg-blue-300 h-1.5 group-hover:h-3 transition-height duration-200 ease-in-out"></i></a>
                 <span className="inline font-poppins"><Typical
-                  loop={Infinity}
+                  loop={2}
                   wrapper="n"
                   steps={[
-                    ' websites.',
-                    4000,
-                    ' interfaces.',
-                    4000,
+                    2250,
                     ' experiences.',
-                    4000
+                    2250,
+                    ' interfaces.',
+                    2250,
+                    ' websites.',
+                    8000
                   ]}
                 /></span></span>
-              <span className="font-normal text-sm">I play with fonts and files, colors and code, design systems and JavaScript frameworks. I wear many hats along the product life-cycle, and I'll help you figure out where you need me the most.</span>
-              <span className="font-normal text-sm">I play with fonts and files, colors and code, design systems and JavaScript frameworks. I wear many hats along the product life-cycle, and I'll help you figure out where you need me the most.</span>
+              <span className="font-normal text-normal leading-8">I slot-in throughout the digital product life-cycle; taking a big-picture approach to projects, I strive to contribute at every stage, from concept to delivery, there's always room for improvement.</span>
             </div>
 
             <div className="flex flex-row space-x-8 text-gray-500 text-base">
@@ -42,9 +54,12 @@ function App() {
           </div>
         </div>
 
-        <div className="relative w-72 bg-yellow-100 pb-5/12 flex-none">
-          <img className="absolute top-0 h-full w-full object-cover content-bottom" src="https://images.pexels.com/photos/196646/pexels-photo-196646.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="" />
-        </div>
+        <animated.div style={{
+          transform: spring.myXTranslateValue.interpolate(myXTranslateValue => `translate(${myXTranslateValue}px)`),
+        }} className="relative w-72 bg-yellow-100 pb-5/12 flex-none">
+          <img className="absolute top-0 h-full w-full object-cover content-end border-4 border-gray-700 hover:shadow-lg transition-all duration-300 " src="https://images.pexels.com/photos/196646/pexels-photo-196646.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="" />
+        </animated.div>
+
 
       </main>
 
