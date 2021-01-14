f3376806717e95acedbde3c59075567e73ec9e75
ArcticJunkie <pikka99@gmail.com>
2021-01-15 01:31:20 +0530

    fixed links, resume needs link updates

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,8 +8,8 @@ function Nav() {
                 <ul className="hidden md:flex flex-row justify-evenly space-x-12 font-mono ">
                     <li className="hover:underline hover:text-black"><a href="https://www.linkedin.com/in/nav-verma/" target="_blank">LinkedIn</a></li>
                     <li className="hover:underline hover:text-black"><a href="https://dribbble.com/navs_lists" target="_blank">Dribbble</a></li>
-                    <li className="hover:underline hover:text-black"><a href="https://github.com/ArcticJunkie" target="_blank">GitHub</a></li>
-                    <li className="hover:underline hover:text-black"><a href="https://drive.google.com/file/d/1Y_uy5qveuIf9GfGZcfIXKGOIH1DRb_PM/view?usp=sharing" target="_blank">Resume</a></li>
+                    <li className="hover:underline hover:text-black"><a href="https://github.com/navs-github" target="_blank">GitHub</a></li>
+                    <li className="hover:underline hover:text-black"><a href="https://drive.google.com/file/d/1g5lcxMaumEyiMUWeHU7QqDgkpzPcKqf0/view?usp=sharing" target="_blank">Resume</a></li>
                 </ul>
             </nav>
         </div>
