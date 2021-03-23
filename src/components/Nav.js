20bb7e8d7965941d83f4b771849c9bf77d75d95e
ArcticJunkie <pikka99@gmail.com>
2021-03-23 15:22:37 +0530

    text to icons in mobile mode

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,13 +3,19 @@
 function Nav() {
     return (
         <div className="Nav text-gray-700">
-            <nav className="bg-blue-50 flex flex-row items-center justify-between px-8 py-8 md:px-52 md:py-8">
-                <div className="font-bold font-mono text-2xl hover:text-blue-700"><a href="#" className="">AV</a></div>
+            <nav className="bg-blue-50 flex flex-row items-center justify-between px-8 py-8 md:px-36 lg:px-52 md:py-8">
+                <div className="font-bold font-mono text-2xl hover:text-blue-700 mr-16 md:mr-52"><a href="#" className="">AV</a></div>
                 <ul className="hidden md:flex flex-row justify-evenly space-x-12 font-mono ">
                     <li className="hover:underline hover:text-black"><a href="https://www.linkedin.com/in/nav-verma/" target="_blank">LinkedIn</a></li>
                     <li className="hover:underline hover:text-black"><a href="https://dribbble.com/navs_lists" target="_blank">Dribbble</a></li>
                     <li className="hover:underline hover:text-black"><a href="https://github.com/navs-github" target="_blank">GitHub</a></li>
                     <li className="hover:underline hover:text-black"><a href="https://drive.google.com/file/d/1g5lcxMaumEyiMUWeHU7QqDgkpzPcKqf0/view?usp=sharing" target="_blank">Resume</a></li>
+                </ul>
+                <ul className="flex md:hidden flex-row items-center justify-evenly space-x-8 text-2xl">
+                    <li className=""><a href="" className=""><i class="fab fa-linkedin"></i></a></li>
+                    <li className=""><a href="" className=""><i class="fab fa-dribbble-square"></i></a></li>
+                    <li className=""><a href="" className=""><i class="fab fa-github-square"></i></a></li>
+                    <li className=""><a href="" className=""><i class="fas fa-file-pdf"></i></a></li>
                 </ul>
             </nav>
         </div>
