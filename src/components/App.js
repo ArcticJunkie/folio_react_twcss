c1c1d91e2c1a8c76f44871783dddac1464a70dbf
ArcticJunkie <pikka99@gmail.com>
2021-03-26 21:51:43 +0530

    space y responsive adjustments, navbar mar/pad

--- src/components/App.js
+++ src/components/App.js
@@ -11,9 +11,9 @@ function App() {
       <main className="flex flex-col space-y-8 justify-between px-8 pt-10 md:px-36 md:pt-20 lg:px-52 lg:pt-28 xl:px-96 xl:pt-32 md:flex-row md:space-y-0 md:space-x-16  text-gray-800 text-xl font-poppins">
 
         {/* landing -> hero text */}
-        <div className="flex justify-between flex-col space-y-6">
+        <div className="flex justify-between flex-col space-y-6 xl:space-y-8">
           <div className="flex flex-col space-y-10">
-            <div className="flex flex-col space-y-6 font-semibold">
+            <div className="flex flex-col space-y-6 xl:space-y-8 font-semibold">
               <span className="text-lg md:text-xl xl:text-2xl">Abhinav Verma</span>
               <span className="text-base md:text-xl xl:text-2xl">I  {' '}
                 <a href="https://dribbble.com/navs-dribbble" target="_blank" className="group relative"><span className="relative z-20 italic font-semibold group-hover:text-blue-800 ">design</span><i className="absolute opacity-60 z-10 left bottom-1 inset-x-0 bg-blue-300 h-1.5 group-hover:h-3 transition-height duration-200 ease-in-out "></i></a>
@@ -23,7 +23,7 @@ function App() {
               <span className="font-normal text-base md:text-xl xl:text-2xl leading-relaxed">I slot-in throughout the digital product life-cycle; taking a big-picture approach to projects, I strive to contribute at every stage, from concept to delivery.</span>
             </div>
 
-            <div className="flex flex-row space-x-8 text-gray-500 text-sm md:text-base xl:text-lg whitespace-nowrap">
+            <div className="flex flex-row space-x-8 xl:space-x-10 text-gray-500 text-sm md:text-base xl:text-lg whitespace-nowrap">
               <span className="">BRAND-STRATEGY</span>
               <span className="">DESIGN</span>
               <span className="">DEVELOPMENT</span>
--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -16,7 +16,7 @@ function HeroImage() {
     return (
         <animated.div style={{
             transform: spring.myXTranslateValue.interpolate(myXTranslateValue => `translate(${myXTranslateValue}px)`),
-        }} className="hidden md:flex flex-col relative w-72 xl:w-96 pb-5/12 flex-none h-128">
+        }} className="hidden md:flex flex-col relative w-72 xl:w-96 pb-5/12 flex-none h-96">
             <img className="absolute top-0 h-full w-full object-cover content-end border-2 border-gray-700 shadow-lg hover:border-blue-900 transition-all duration-300 " src="https://images.pexels.com/photos/196646/pexels-photo-196646.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="" />
         </animated.div>
     );
