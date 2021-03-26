59de3a05706d08884db8ab076678ccec8b02c59d
ArcticJunkie <pikka99@gmail.com>
2021-03-26 21:46:41 +0530

    fix navbar sizing

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,8 +4,8 @@ function Nav() {
     return (
         <div className="Nav text-gray-700">
             <nav className="bg-blue-50 flex flex-row items-center justify-between px-8 py-8 md:px-36 lg:px-52 md:py-8">
-                <div className="font-bold font-dmono text-2xl hover:text-blue-700 mr-16 md:mr-52"><a href="#" className="">AV</a></div>
-                <ul className="hidden md:flex flex-row justify-evenly space-x-12 font-dmono ">
+                <div className="font-bold font-dmono text-2xl xl:text-3xl hover:text-blue-700 mr-16 md:mr-52"><a href="#" className="">AV</a></div>
+                <ul className="hiddenmd:flex flex-row justify-evenly space-x-12 font-dmono  text-base xl:-text-lg">
                     <li className="hover:underline hover:text-black"><a href="https://www.linkedin.com/in/nav-verma/" target="_blank">LinkedIn</a></li>
                     <li className="hover:underline hover:text-black"><a href="https://dribbble.com/navs-dribbble" target="_blank">Dribbble</a></li>
                     <li className="hover:underline hover:text-black"><a href="https://github.com/navs-github" target="_blank">GitHub</a></li>
