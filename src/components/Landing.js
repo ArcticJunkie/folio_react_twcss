a76546283bdffca4ca698ad551b554e11d62ca0c
ArcticJunkie <pikka99@gmail.com>
2021-05-04 00:16:11 +0530

    landing pt fix

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -9,7 +9,7 @@ function Landing() {
     return (
         <div className="App bg-gray-50">
             {/* px-8 pt-10 md:px-36 md:pt-12 lg:px-52 lg:pt-16 xl:px-96 xl:pt-32 */}
-            <main className="pb-40 flex flex-col space-y-20 justify-between px-8 pt-10 xl:px-52 xl:pt-32   text-gray-800 text-xl font-poppins">
+            <main className="pb-40 flex flex-col space-y-20 justify-between px-8 pt-16 xl:px-52 xl:pt-32   text-gray-800 text-xl font-poppins">
 
                 {/* landing -> hero text */}
                 <div className="flex justify-between flex-col space-y-6 xl:space-y-8">
